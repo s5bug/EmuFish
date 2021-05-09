866983d3cff4ef43bbdc13851bd6e451e4136f03
Aly <me@s5.pm>
2021-05-08 23:04:14 -0700

    Add core library separate from UI library

---
+++ EmuFish.Core/Input.fs
@@ -0,0 +1,7 @@
+﻿module EmuFish.Core.Input
+
+type Pad(inputChoices: array<InputChoice>) = class end
+and InputChoice =
+    | Digital of buttonName: string
+    | Analog of axisName: string
+    | Extension of choices: Map<string, Pad>
---
+++ EmuFish.Core/Script.fs
@@ -0,0 +1,9 @@
+﻿module EmuFish.Core.Script
+
+open NLua
+
+type Script(origin: string, state: Lua) =
+    static member Open (path: string) : Script =
+        let lua = new Lua()
+        lua.DoFile path |> ignore
+        Script(path, lua)
