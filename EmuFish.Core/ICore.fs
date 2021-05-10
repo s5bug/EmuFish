e00becdc677111ddb245b4bdc276facb03f57040
Aly <me@s5.pm>
2021-05-10 00:54:59 -0700

    Start on cores and plugin loading

---
+++ EmuFish.Core/ICore.fs
@@ -0,0 +1,11 @@
+﻿namespace EmuFish.Core
+
+type ICore<'state> =
+    interface
+    
+    abstract member Initialize : unit -> unit
+    abstract member SupportsRom : string -> bool
+    
+    abstract member Load : string -> 'state
+    
+    end
---
+++ EmuFish.SharkBoy/SharkBoy.fs
@@ -0,0 +1,10 @@
+﻿namespace EmuFish.SharkBoy
+
+open EmuFish.Core
+
+type SharkBoy =
+    interface ICore<bool> with
+        member this.Initialize () = ()
+        member this.SupportsRom name = name.EndsWith(".gb")
+        member this.Load name = true
+    
